5491ed1986f9dcb4bf6871d7f5150488daa60204
Pyae Sone Maung <pyaesonemaung.psm1996@gmail.com>
2021-07-19 13:30:36 +0630

    updating code and new feautre

---
+++ js/profile.js
@@ -0,0 +1,14 @@
+var modal = document.getElementById("myModal");
+var modalImg = document.getElementById("img01");
+
+function getImage(data){
+    var images = data.src;
+    modal.style.display = "block";
+    modalImg.src = images;
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() { 
+  modal.style.display = "none";
+}
