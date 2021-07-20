585456bfcb501dbc243276003af5d2d9b0ce193e
Pyae Sone Maung <pyaesonemaung.psm1996@gmail.com>
2021-07-20 11:26:51 +0630

    fix smtp code

--- js/profile.js
+++ js/profile.js
@@ -14,5 +14,17 @@ span.onclick = function() {
 }
 
 function sendEmail(){
-  alert("Message send successfully")
+  Email.send({
+    Host : "smtp.gmail.com",
+    Username : "blackmango.bm1990@gmail.com",
+    Password : "aascbsdnmzmcseoh",
+    To : 'pyaesonemaung.psm1996@gmail.com',
+    From : document.getElementById("contact-email").value,
+    Subject : "Contact From Profile",
+    Body : document.getElementById("contact-message").value
+}).then(
+  message => alert("Message sent successfully")
+);
+document.getElementById("contact-email").value = "";
+document.getElementById("contact-message").value = "";
 }
